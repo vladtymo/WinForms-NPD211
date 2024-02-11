09855632f93529dcea2d269d0817b1161dcd5b5a
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-02-11 15:42:40 +0200

    Timer example

--- 06_Child_Controls/Form1.cs
+++ 06_Child_Controls/Form1.cs
@@ -1,10 +1,23 @@
+using Timer = System.Windows.Forms.Timer;
+
 namespace _06_Child_Controls
 {
     public partial class Form1 : Form
     {
+        private Timer timer = new();
         public Form1()
         {
             InitializeComponent();
+
+            timer.Interval = 500;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private int count = 1;
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            this.Text = (++count).ToString();
         }
 
         private void StartBtnClick(object sender, EventArgs e)
